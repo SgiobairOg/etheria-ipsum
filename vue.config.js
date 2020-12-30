ba4598daf5e072a1ac2ca3245b0c785bbed81887
Jason Wilson <jason.wilson@valtech.com>
2020-12-30 03:42:55 +0100

    feat(): Enable Compression

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,24 @@ module.exports = {
   },
   configureWebpack: {
     stats: 'errors-only'
+  },
+  pluginOptions: {
+    compression:{
+      brotli: {
+        filename: '[path].br[query]',
+        algorithm: 'brotliCompress',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          level: 11,
+        },
+        minRatio: 0.8,
+      },
+      gzip: {
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+      }
+    }
   }
 }
