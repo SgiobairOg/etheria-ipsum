a901d456438f66c95f7aa0f57018a7e8c85273b1
Jason Wilson <jason.wilson@valtech.com>
2021-01-12 01:48:33 +0100

    feat(): Add Paragraph Breaking System

--- src/http/get-dialog-words-000count/index.js
+++ src/http/get-dialog-words-000count/index.js
@@ -4,6 +4,7 @@ const randomItem = require('random-item')
 
 const dialogData = dialog
 const ROOT = process.env.ROOT || 'http://localhost:3333'
+const PARAGRAPH_BREAK = '||X||'
 
 exports.handler = async function http (req) {
   const result = {
@@ -11,6 +12,7 @@ exports.handler = async function http (req) {
     words: 0,
     characters: 0,
     charactersExcludingSpaces: 0,
+    paragraphs: 1,
     self: req.url,
     ...routeList(ROOT),
   }
@@ -28,6 +30,8 @@ exports.handler = async function http (req) {
     }
   }
 
+  let lineCount = 0
+
   do {
     let currentLine
     const remainingWordCount = targetWordCountInt - result.words
@@ -41,9 +45,19 @@ exports.handler = async function http (req) {
     result.words += currentLine.words
     result.characters += 1 + currentLine.characters
     result.charactersExcludingSpaces += currentLine.charactersExcludingSpaces
+
+    lineCount += 1
+
+    if(lineCount === randomItem([5,6,7,8]) || lineCount >= 8) {
+      result.dialog += PARAGRAPH_BREAK
+      result.characters += 1
+      result.paragraphs += 1
+      lineCount = 0
+    }
   } while ( targetWordCountInt > result.words )
 
   result.dialog.trim()
+  result.dialog = result.dialog.split(PARAGRAPH_BREAK)
 
   return {
     statusCode: 200,
